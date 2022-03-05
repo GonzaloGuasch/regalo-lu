8406b0396309d3a5c7e51120f0bc7fc630c3a7b8
GuaschGonzalo <gonzalo.guasch@mercadolibre.com>
2022-03-05 20:50:23 -0300

    regalo idea

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Confetti from 'react-confetti'
 import './App.css';
 
 function App() {
+  const [showConfetti, setSwowConfetti] = useState(false)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!showConfetti &&
+          <img 
+            src="https://freepikpsd.com/file/2019/10/present-png-icon-1-1-Transparent-Images.png"
+            alt="new"
+            onClick={() => setSwowConfetti(!showConfetti)}
+          />
+        }
+        {showConfetti &&
+          <React.Fragment>
+              <Confetti
+                width={window.screen.width}
+                height={window.screen.height}
+              />
+              <div className='card-body'>
+                <img src='https://c.tenor.com/w9t5gL4hnK0AAAAC/carlton-dance.gif'></img>
+
+                <div class="box">
+                  <div class='inner'>
+                    <h1>Valido por un regalo</h1>
+                  <div class='info clearfix'>
+                    <div class='wp'>Monto<h2>6k - 8k</h2><h2>2K</h2></div>
+                    <div class='wp'>Moneda<h2>Pesos</h2><h2>PESOS</h2></div>
+                    <div class='wp'>Concepto<h2>BDAY</h2><h2>Gorra papa</h2></div>
+                  </div>
+                  <div class='total clearfix'>
+                    <h2>Total : <p>$ 10.000</p></h2>
+                  </div>
+                </div>
+                </div>
+              </div>
+          </React.Fragment>
+
+        }
       </header>
     </div>
   );
